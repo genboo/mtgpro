e06b6df042176fbca42d44880ef0e25cd8ffe0ce
genboo <gen.masj@gmail.com>
2018-07-12 16:57:06 +0300

    fix: expand max height

--- app/src/main/java/ru/spcm/apps/mtgpro/view/components/ExpandableLinearLayout.kt
+++ app/src/main/java/ru/spcm/apps/mtgpro/view/components/ExpandableLinearLayout.kt
@@ -3,6 +3,7 @@ package ru.spcm.apps.mtgpro.view.components
 import android.content.Context
 import android.transition.Transition
 import android.util.AttributeSet
+import android.util.DisplayMetrics
 import android.widget.LinearLayout
 
 class ExpandableLinearLayout(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
@@ -10,6 +11,8 @@ class ExpandableLinearLayout(context: Context, attrs: AttributeSet) : LinearLayo
     private var collapsedHeight = 0
     private var isExpanded = false
     private var isAnimated = false
+
+    private var maxHeight = 100
 
     private val listener = object : ExpandListener() {
         override fun onTransitionEnd(transition: Transition) {
@@ -23,6 +26,11 @@ class ExpandableLinearLayout(context: Context, attrs: AttributeSet) : LinearLayo
 
     }
 
+    init {
+        val metrics = resources.displayMetrics
+        maxHeight = 250 * (metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT)
+    }
+
     fun toggle() {
         if (!isAnimated) {
             val params = layoutParams
@@ -30,7 +38,7 @@ class ExpandableLinearLayout(context: Context, attrs: AttributeSet) : LinearLayo
                 params.height = collapsedHeight
             } else {
                 collapsedHeight = height
-                params.height = 500
+                params.height = maxHeight
             }
             isExpanded = !isExpanded
             layoutParams = params
